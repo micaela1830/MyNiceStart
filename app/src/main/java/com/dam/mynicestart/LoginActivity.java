1492a21b35920a6b89e57693b26469306e31b0e9
micaela18 <micaela_dayana18@hotmail.com>
2020-11-20 12:43:17 +0100

    logear,splash,signUp

--- app/src/main/java/com/dam/mynicestart/LoginActivity.java
+++ app/src/main/java/com/dam/mynicestart/LoginActivity.java
@@ -2,13 +2,29 @@ package com.dam.mynicestart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView mSignUpTextview = (TextView)findViewById(R.id.signup_text);
+
+        mSignUpTextview.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
