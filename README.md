6572537b936fa4922e4b461d8c4195030a3863a7
micaela18 <micaela_dayana18@hotmail.com>
2020-12-09 17:44:21 +0100

    logear,splash,signUp,main,main2,fadein,font,rotate,alpha,statelist, javadoc, readme,screenshoots,glide

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # MyNiceStart
 [mica en github](https://github.com/micaela1830)
 
-**Splash** | **Login** | **Sign Up** | **Main** | **Main2**
------- | ----- | ------ | ------ | ------
-![](img/splash.jpg) | ![](img/login.jpg) | ![](img/signup.jpg)| ![](img/main.jpg)| ![](img/main2.jpg)
+**Splash** | **Login** | **Sign Up** |
+------ | ----- | ------ |
+![](img/splash.jpg) | ![](img/login.jpg) | ![](img/signup.jpg)|
 
-
-
+ **Main** | **Main2**
+ ------ | ------
+ ![](img/main.jpg)| ![](img/main2.jpg)
--- app/src/main/java/com/dam/mynicestart/LoginActivity.java
+++ app/src/main/java/com/dam/mynicestart/LoginActivity.java
@@ -3,9 +3,15 @@ package com.dam.mynicestart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
 /**
  * Ventana que redirecciona a signup
  * @author micaela18
@@ -13,10 +19,14 @@ import android.widget.TextView;
  */
 public class LoginActivity extends AppCompatActivity {
 
+    ImageView imgPerro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        imgPerro = findViewById(R.id.Perro);
 
         TextView mSignUpTextview = (TextView)findViewById(R.id.signup_text);
 
@@ -29,6 +39,15 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+       // String url = "https://images.unsplash.com/photo-1603755043619-9b94c368f4db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+
+        Glide.with(this)
+                .load(R.drawable.perrito)
+                .circleCrop()
+                .transition(DrawableTransitionOptions.withCrossFade(500))
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.colorPrimaryDark)))
+//                .placeholder(R.drawable.bg2)
+                .into(imgPerro);
     }
 
     public void login(View view) {
